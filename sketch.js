ee18c03788ce1852ab76d66aef1c6c457ea35080
trisha-n <trisha2006mummy@gmail.com>
2020-07-28 15:50:52 +0530

    bg and score

--- sketch.js
+++ sketch.js
@@ -5,13 +5,16 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var backgroundImg,backgroundImg1,platform,bg;
 var bird, slingshot;
+var score = 0;
 
 var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+    backgroundImg1 = loadImage("sprites/bg2.jpg");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +48,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(bg){
+    background(bg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score:"+score,600,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -84,5 +95,18 @@ function mouseReleased(){
 function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
+    }
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/kolkata");
+    var responseJson = await response.json()
+    var hour = responseJson.datetime.slice(11,13);
+    console.log(hour);
+    hour = 19;
+    if(hour >= 6 && hour <= 18){
+      bg = backgroundImg;
+    }else{
+        bg = backgroundImg1;
     }
 }
